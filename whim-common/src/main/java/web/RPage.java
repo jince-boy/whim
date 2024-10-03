4c304265e2501bb4cc9338b06c62d836962d12be
Jin Ce <jince_hm@163.com>
2024-10-04 00:40:39 +0800

    feat: 统一响应对象，分页对象

---
+++ whim-common/src/main/java/web/RPage.java
@@ -0,0 +1,37 @@
+package web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Jince
+ * date: 2024/10/4 00:36
+ * description: 分页对象
+ */
+@Data
+@AllArgsConstructor
+public class RPage<T> {
+    /**
+     * 当前页
+     */
+    private Long currentPage;
+    /**
+     * 分页数据
+     */
+    private List<T> data;
+    /**
+     * 总页数
+     */
+    private Long pages;
+    /**
+     * 每页数量
+     */
+    private Long size;
+    /**
+     * 总数量
+     */
+    private Long total;
+}
+
